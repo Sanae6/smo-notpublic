84899a2229df4e2664ed3339cc84565844e0c40c
Sanae <atmatm6000@outlook.com>
2023-09-22 16:21:05 -0400

    Clean up

--- src/program/main.cpp
+++ src/program/main.cpp
@@ -10,11 +10,6 @@
 
 #include <basis/seadRawPrint.h>
 #include <prim/seadSafeString.h>
-#include <resource/seadResourceMgr.h>
-#include <filedevice/nin/seadNinSDFileDeviceNin.h>
-#include <filedevice/seadFileDeviceMgr.h>
-#include <filedevice/seadPath.h>
-#include <resource/seadArchiveRes.h>
 #include <heap/seadHeapMgr.h>
 #include <devenv/seadDebugFontMgrNvn.h>
 #include <gfx/seadTextWriter.h>
@@ -121,110 +116,12 @@ void drawDebugWindow() {
     ImGui::End();
 }
 
-HOOK_DEFINE_TRAMPOLINE(ControlHook) {
-    static void Callback(StageScene *scene) {
-        Orig(scene);
-    }
-};
-
 HOOK_DEFINE_REPLACE(ReplaceSeadPrint) {
     static void Callback(const char *format, ...) {
         va_list args;
         va_start(args, format);
         Logger::log(format, args);
         va_end(args);
-    }
-};
-
-HOOK_DEFINE_TRAMPOLINE(CreateFileDeviceMgr) {
-    static void Callback(sead::FileDeviceMgr *thisPtr) {
-
-        Orig(thisPtr);
-
-        thisPtr->mMountedSd = nn::fs::MountSdCardForDebug("sd").isSuccess();
-
-        sead::NinSDFileDevice *sdFileDevice = new sead::NinSDFileDevice();
-
-        thisPtr->mount(sdFileDevice);
-    }
-};
-
-HOOK_DEFINE_TRAMPOLINE(RedirectFileDevice) {
-    static sead::FileDevice *
-    Callback(sead::FileDeviceMgr *thisPtr, sead::SafeString &path, sead::BufferedSafeString *pathNoDrive) {
-
-        sead::FixedSafeString<32> driveName;
-        sead::FileDevice *device;
-
-        // Logger::log("Path: %s\n", path.cstr());
-
-        if (!sead::Path::getDriveName(&driveName, path)) {
-
-            device = thisPtr->findDevice("sd");
-
-            if (!(device && device->isExistFile(path))) {
-
-                device = thisPtr->getDefaultFileDevice();
-
-                if (!device) {
-                    Logger::log("drive name not found and default file device is null\n");
-                    return nullptr;
-                }
-
-            } else {
-                Logger::log("Found File on SD! Path: %s\n", path.cstr());
-            }
-
-        } else
-            device = thisPtr->findDevice(driveName);
-
-        if (!device)
-            return nullptr;
-
-        if (pathNoDrive != nullptr)
-            sead::Path::getPathExceptDrive(pathNoDrive, path);
-
-        return device;
-    }
-};
-
-HOOK_DEFINE_TRAMPOLINE(FileLoaderLoadArc) {
-    static sead::ArchiveRes *
-    Callback(al::FileLoader *thisPtr, sead::SafeString &path, const char *ext, sead::FileDevice *device) {
-
-        // Logger::log("Path: %s\n", path.cstr());
-
-        sead::FileDevice *sdFileDevice = sead::FileDeviceMgr::instance()->findDevice("sd");
-
-        if (sdFileDevice && sdFileDevice->isExistFile(path)) {
-
-            Logger::log("Found File on SD! Path: %s\n", path.cstr());
-
-            device = sdFileDevice;
-        }
-
-        return Orig(thisPtr, path, ext, device);
-    }
-};
-
-sead::FileDevice *tryFindNewDevice(sead::SafeString &path, sead::FileDevice *orig) {
-    sead::FileDevice *sdFileDevice = sead::FileDeviceMgr::instance()->findDevice("sd");
-
-    if (sdFileDevice && sdFileDevice->isExistFile(path))
-        return sdFileDevice;
-
-    return orig;
-}
-
-HOOK_DEFINE_TRAMPOLINE(FileLoaderIsExistFile) {
-    static bool Callback(al::FileLoader *thisPtr, sead::SafeString &path, sead::FileDevice *device) {
-        return Orig(thisPtr, path, tryFindNewDevice(path, device));
-    }
-};
-
-HOOK_DEFINE_TRAMPOLINE(FileLoaderIsExistArchive) {
-    static bool Callback(al::FileLoader *thisPtr, sead::SafeString &path, sead::FileDevice *device) {
-        return Orig(thisPtr, path, tryFindNewDevice(path, device));
     }
 };
 
@@ -279,24 +176,16 @@ extern "C" void exl_main(void *x0, void *x1) {
     /* Setup hooking enviroment. */
     exl::hook::Initialize();
 
-    nn::os::SetUserExceptionHandler(exception_handler, nullptr, 0, nullptr);
-    installExceptionStub();
+    handler::installExceptionHandler([](handler::ExceptionInfo& info) {
+        handler::printCrashReport(info);
+        return false;
+    });
 
-    Logger::instance().init(LOGGER_IP, 3080);
+    Logger::instance().init(LOGGER_IP, 3085);
 
     runCodePatches();
 
     GameSystemInit::InstallAtOffset(0x535850);
-
-    // SD File Redirection
-
-    RedirectFileDevice::InstallAtOffset(0x76CFE0);
-    FileLoaderLoadArc::InstallAtOffset(0xA5EF64);
-    CreateFileDeviceMgr::InstallAtOffset(0x76C8D4);
-    FileLoaderIsExistFile::InstallAtSymbol(
-            "_ZNK2al10FileLoader11isExistFileERKN4sead14SafeStringBaseIcEEPNS1_10FileDeviceE");
-    FileLoaderIsExistArchive::InstallAtSymbol(
-            "_ZNK2al10FileLoader14isExistArchiveERKN4sead14SafeStringBaseIcEEPNS1_10FileDeviceE");
 
     // Sead Debugging Overriding
 
@@ -305,10 +194,6 @@ extern "C" void exl_main(void *x0, void *x1) {
     // Debug Text Writer Drawing
 
     DrawDebugMenu::InstallAtOffset(0x50F1D8);
-
-    // General Hooks
-
-    ControlHook::InstallAtSymbol("_ZN10StageScene7controlEv");
 
     // ImGui Hooks
 #if IMGUI_ENABLED
